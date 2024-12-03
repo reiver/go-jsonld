95307ca86573b825c475d49318c9839f648db655
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-03 08:46:13 -0800

    jsonld.ContextOf()

--- contextof.go
+++ contextof.go
@@ -6,6 +6,18 @@ import (
 	"github.com/reiver/go-erorr"
 )
 
+// ContextOf returns the JSON-LD context.
+//
+// For example:
+//
+//	type MyStruct struct {
+//		NameSpace jsonld.NameSpace `jsonld:"http://example.com/ns#"`
+//		Preix     jsonld.Prefix    `jsonld:"ex"`
+//		
+//		Apple  string `jsonld:"apple"`
+//		Banana int    `jsonld:"banana"`
+//		Cherry bool   `jsonld:"cherry"`
+//	}
 func ContextOf(value any) (Context, error) {
 	if nil == value {
 		var nada Context
