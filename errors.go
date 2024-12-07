aa58ec5f97c205f76bae05ff9cd6dc0c8f390ba4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-07 03:41:52 -0800

    refactoring to mostly use github.com/reiver/go-json (and go-jsonld mostly deals with context)

--- errors.go
+++ errors.go
@@ -5,7 +5,6 @@ import (
 )
 
 const (
-	errNilFunc          = erorr.Error("jsonld: nil func")
 	errNilReflectedType = erorr.Error("jsonld: nil reflected-value")
 	errNilValue         = erorr.Error("jsonld: nil value")
 	errNotStruct        = erorr.Error("jsonld: not struct")
