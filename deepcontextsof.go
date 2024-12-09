a538b63e220f5e5e16c132e85657b3b161c84662
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-08 23:25:32 -0800

    jsonld.DeepContextsOf()

---
+++ deepcontextsof.go
@@ -0,0 +1,76 @@
+package jsonld
+
+
+import (
+	"slices"
+	"sort"
+	"reflect"
+
+	"github.com/reiver/go-erorr"
+)
+
+func DeepContextsOf(value any) ([]Context, error) {
+	if nil == value {
+		return nil, errNilValue
+	}
+
+	var contexts []Context
+	var contextsMap map[string]*Context = map[string]*Context{}
+
+	{
+		ctx, err := ContextOf(value)
+		if nil != err {
+			return nil, err
+		}
+
+		{
+			var nada Context
+			if !reflect.DeepEqual(nada, ctx) {
+				contexts = append(contexts, ctx)
+				contextsMap[ctx.NameSpace] = &(contexts[len(contexts)-1])
+			}
+		}
+	}
+
+	{
+		var reflectedType reflect.Type = reflect.TypeOf(value)
+
+		var kind reflect.Kind = reflectedType.Kind()
+		if reflect.Struct != kind {
+			return  nil, erorr.Errorf("jsonld: cannot get context-of something of kind %q", kind)
+		}
+
+		var reflectedStructValue reflect.Value = reflect.ValueOf(value)
+
+		var limit int = reflectedStructValue.NumField()
+
+		for index:=0; index<limit; index++ {
+
+			var fieldValue reflect.Value = reflectedStructValue.Field(index)
+
+			ctxs, err := DeepContextsOf(fieldValue.Interface())
+			if nil != err {
+				return nil, err
+			}
+
+			for _, ctx := range ctxs {
+				var ns string = ctx.NameSpace
+
+				contextPointer, found := contextsMap[ns]
+				if found {
+					contextPointer.Names = append(contextPointer.Names, ctx.Names...)
+					sort.Strings(contextPointer.Names)
+					slices.Compact(contextPointer.Names)
+				} else {
+					var nada Context
+					if !reflect.DeepEqual(nada, ctx) {
+						contexts = append(contexts, ctx)
+						contextsMap[ctx.NameSpace] = &(contexts[len(contexts)-1])
+					}
+				}
+			}
+		}
+	}
+
+	return contexts, nil
+}
---
+++ deepcontextsof_test.go
@@ -0,0 +1,77 @@
+package jsonld_test
+
+import (
+	"testing"
+
+	"reflect"
+
+	"github.com/reiver/go-jsonld"
+)
+
+func TestDeepContextsOf(t *testing.T) {
+
+	tests := []struct{
+		Value any
+		Expected []jsonld.Context
+	}{
+		{
+			Value: struct{
+			}{},
+		},
+
+
+
+		{
+			Value: struct{
+				NameSpace jsonld.NameSpace `jsonld:"http://ns.example/id/42"`
+			}{},
+			Expected: []jsonld.Context{
+				jsonld.Context{
+					NameSpace: `http://ns.example/id/42`,
+				},
+			},
+		},
+
+
+
+		{
+			Value: struct{
+				NameSpace jsonld.NameSpace `jsonld:"http://ns.example/id/42"`
+
+				Something struct{
+					NameSpace jsonld.NameSpace `jsonld:"http://example.com/ns"`
+				} `json:"something,omitempty"`
+			}{},
+			Expected: []jsonld.Context{
+				jsonld.Context{
+					NameSpace: `http://ns.example/id/42`,
+					Names: []string{"something"},
+				},
+				jsonld.Context{
+					NameSpace: `http://example.com/ns`,
+				},
+			},
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		actual, err := jsonld.DeepContextsOf(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error but actually got one.", testNumber)
+			t.Logf("ERROR: (%T) %s", err, err)
+			t.Logf("VALUE: (%T) %#v", test.Value, test.Value)
+			continue
+		}
+
+		expected := test.Expected
+
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("For test #%d, the actual contexts are not what was expected.", testNumber)
+			t.Logf("EXPECTED: (%d)\n%#v", len(expected), expected)
+			t.Logf("ACTUAL:   (%d)\n%#v", len(actual), actual)
+			t.Logf("VALUE: (%T) %#v", test.Value, test.Value)
+			continue
+		}
+	}
+}
